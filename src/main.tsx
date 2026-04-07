ff9dd824947f3b4e37b1f42f699b94a07979314f
Lucia Nazzaro <lucia.nazzaro@live.it>
2026-04-07 23:01:02 +0300

    chore: update ui consumer css setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@mahdava/ui/tw-inline-source.css'
-import '@mahdava/ui/styles.css'
-import '@mahdava/ui/themes/default.css'
-import './index.css'
+import './ui.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
